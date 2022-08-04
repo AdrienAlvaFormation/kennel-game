362f0059277627e7c834e7480c087763698c1522
Alvarez <jamespatageules@hotmail.com>
2022-08-04 23:27:45 +0200

    Gaming phase timer + refacto draw methods

--- core/src/fr/adrien/sandbox/controller/GamePhaseTimer.java
+++ core/src/fr/adrien/sandbox/controller/GamePhaseTimer.java
@@ -6,8 +6,8 @@ public class GamePhaseTimer {
 
     private float time;
     private float counter = time;
-    private final int MIN_TIME = 5;
-    private final int MAX_TIME = 10;
+    private final int MIN_TIME = 8;
+    private final int MAX_TIME = 12;
     private int RANGE = MAX_TIME - MIN_TIME + 1;
 
     //Phase Booleans
@@ -16,12 +16,13 @@ public class GamePhaseTimer {
     private boolean isWatching;
 
     public GamePhaseTimer() {
-        this.newPhase = true;
 
         this.setRandomTimer();
+
     }
 
     public void update(float delta) {
+
 
         counter -= Gdx.graphics.getDeltaTime();
         //if you have delta passed as a overload you can do
@@ -29,22 +30,41 @@ public class GamePhaseTimer {
 
         System.out.println(counter);
 
-        if (counter <= 3)
-        {
-            //Play annoying sound to make you stress even more
-            System.out.println("NEAR END !");
+        if(counter > 6) {
+
+            this.isNotWatching = true;
+            this.isReturning = false;
+            this.isWatching = false;
+
         }
 
-        if (counter <= 0)
+        if (counter <= 6 && counter > 4)
         {
-            //Time is up!
+            System.out.println("NEAR END !");
+
+            this.isNotWatching = false;
+            this.isReturning = true;
+            this.isWatching = false;
+
+        }
+
+        if (counter <= 4 && counter > 0)
+        {
+
             System.out.println("THE EEEEEEEEEEEEEEEND");
+
+            this.isNotWatching = false;
+            this.isReturning = false;
+            this.isWatching = true;
+
+        }
+
+        if (counter <= 0) {
             //You can just reset the counter by setting it equal to time again.
             //But there will almost always be left over (counter will be less then 0)
             //So if this is important (probably not for a quiz game)
-//            counter = time + -counter; //Add the time this round took too long.
+            //counter = time + -counter; //Add the time this round took too long.
             setRandomTimer();
-
         }
     }
 
@@ -53,4 +73,27 @@ public class GamePhaseTimer {
         this.counter = time;
     }
 
+    public boolean isNotWatching() {
+        return isNotWatching;
+    }
+
+    public void setNotWatching(boolean notWatching) {
+        isNotWatching = notWatching;
+    }
+
+    public boolean isReturning() {
+        return isReturning;
+    }
+
+    public void setReturning(boolean returning) {
+        isReturning = returning;
+    }
+
+    public boolean isWatching() {
+        return isWatching;
+    }
+
+    public void setWatching(boolean watching) {
+        isWatching = watching;
+    }
 }// Eo GamePhaseTimer class
--- core/src/fr/adrien/sandbox/screens/GameScreen.java
+++ core/src/fr/adrien/sandbox/screens/GameScreen.java
@@ -19,14 +19,19 @@ public class GameScreen implements Screen {
 
     final Sandbox game;
     public boolean hasWon;
+    public boolean hasLost;
     public OrthographicCamera camera;
     private LevelBackground lvlBackground;
     private Dog dog;
+    private float playerXBuffer;
+    private float playerYBuffer;
     private Reaper reaper;
     private GlyphLayout glyphLayout[];
     private final String GRASS_BACKGROUND_FILENAME = "grass.png";
     private TextureRegion frameBuffer = null;
     private GamePhaseTimer timer;
+
+
 
     // CONSTRUCTOR
 
@@ -41,13 +46,16 @@ public class GameScreen implements Screen {
 
         this.lvlBackground = new LevelBackground(800, 1600, GRASS_BACKGROUND_FILENAME, camera.viewportHeight);
 
-        this.dog = new Dog(200, 150, Math.round(camera.viewportWidth / 2), Math.round(camera.viewportHeight / 2));
+//        this.dog = new Dog(200, 150, Math.round(camera.viewportWidth / 2), Math.round(camera.viewportHeight / 2));
+
+        this.dog = new Dog(200, 150, 100, Math.round(camera.viewportHeight / 2));
 
         this.reaper = new Reaper(100, 100, 1400, Math.round(camera.viewportHeight / 2 - 50));
 
-        this.glyphLayout = new GlyphLayout[1];
+        this.glyphLayout = new GlyphLayout[2];
 
         glyphLayout[0]=new GlyphLayout(game.font, "VICTOIRE !");
+        glyphLayout[1]=new GlyphLayout(game.font, "DEFAITE...");
 
         this.timer = new GamePhaseTimer();
 
@@ -87,62 +95,41 @@ public class GameScreen implements Screen {
 
         dog.flip(); // flip character sprite to look at movement direction
 
-        game.batch.draw(
-            lvlBackground.getBackgroundImage(),
-            lvlBackground.getBackgroundRectangle().x,
-            lvlBackground.getBackgroundRectangle().y,
-            lvlBackground.getBackgroundRectangle().width,
-            lvlBackground.getBackgroundRectangle().height
-        );
+        // BACKGROUND
 
-        game.batch.draw(
-            lvlBackground.getFinishLine(),
-            lvlBackground.getFinishRectangle().x - 200,
-            lvlBackground.getFinishRectangle().y,
-            lvlBackground.getFinishRectangle().width + 200,
-            lvlBackground.getFinishRectangle().height
-        );
+        drawBackground();
 
-        game.batch.draw(
-            reaper.getOrangeTexture(),
-            reaper.getRectangle().x,
-            reaper.getRectangle().y,
-            reaper.getRectangle().width,
-            reaper.getRectangle().height
-        );
+        // FINISHLINE
 
-        if(dog.getDogRectangle().getX() != dog.getxBuffer() || dog.getDogRectangle().getY() != dog.getyBuffer())  {
-            game.batch.draw(
-                currentFrame,
-                dog.getDogRectangle().getX(),
-                dog.getDogRectangle().getY(),
-                dog.getDogRectangle().getWidth(),
-                dog.getDogRectangle().getHeight()
-            );
+        drawFinishLine();
 
-            frameBuffer = currentFrame; // dans le cas ou le personnage bouge on actualise le frameBuffer
-                                       // pour pouvoir le figer quand il s'arrete.
-        } else {
-            game.batch.draw(
-                    frameBuffer,
-                    dog.getDogRectangle().getX(),
-                    dog.getDogRectangle().getY(),
-                    dog.getDogRectangle().getWidth(),
-                    dog.getDogRectangle().getHeight()
-            );
-        }
+        // CHARACTER
 
+        drawCharacter(currentFrame);
+
+        // REAPER
+
+        drawReaper();
+
+        // VICTORY MESSAGE
         if (dog.getDogRectangle().overlaps(lvlBackground.getFinishRectangle())) {
 
-            for (int i=0;i<glyphLayout.length;i++) {
-                game.font.draw(game.batch, glyphLayout[i], camera.viewportWidth / 2 - (glyphLayout[i].width / 2), camera.viewportHeight / 2 + (glyphLayout[i].height / 2));
-            }
+            game.font.draw(game.batch, glyphLayout[0], camera.viewportWidth / 2 - (glyphLayout[0].width / 2), camera.viewportHeight / 2 + (glyphLayout[0].height / 2));
 
             this.hasWon = true;
 
         }
 
+        // LOOSE MESSAGE
+        if(timer.isWatching() && (playerXBuffer != dog.getDogRectangle().x || playerYBuffer != dog.getDogRectangle().y)){
+
+            game.font.draw(game.batch, glyphLayout[1], camera.viewportWidth / 2 - (glyphLayout[1].width / 2), camera.viewportHeight / 2 + (glyphLayout[1 ].height / 2));
+
+            this.hasLost = true;
+        }
+
         game.batch.end();
+        ////////////////
 
         timer.update(delta);
 
@@ -155,7 +142,7 @@ public class GameScreen implements Screen {
             dog.setyBuffer(dog.getDogRectangle().getY());
         }
 
-        if (!this.hasWon) {
+        if(!this.hasWon) {
             dog.move();
         }
 
@@ -184,9 +171,94 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         lvlBackground.getBackgroundImage().dispose();
+        lvlBackground.getFinishLine().dispose();
         dog.getDogImage().dispose();
-        game.batch.dispose();
         dog.getWalkSheet().dispose();
+        reaper.getGreenTexture().dispose();
+        reaper.getOrangeTexture().dispose();
+        reaper.getRedTexture().dispose();
+        game.batch.dispose();
+    }
+
+    // DRAW METHOD
+
+    private void drawBackground() {
+        game.batch.draw(
+            lvlBackground.getBackgroundImage(),
+            lvlBackground.getBackgroundRectangle().x,
+            lvlBackground.getBackgroundRectangle().y,
+            lvlBackground.getBackgroundRectangle().width,
+            lvlBackground.getBackgroundRectangle().height
+        );
+    }
+
+    private void drawFinishLine() {
+        game.batch.draw(
+            lvlBackground.getFinishLine(),
+            lvlBackground.getFinishRectangle().x - 200,
+            lvlBackground.getFinishRectangle().y,
+            lvlBackground.getFinishRectangle().width + 200,
+            lvlBackground.getFinishRectangle().height
+        );
+    }
+
+    private void drawCharacter(TextureRegion currentFrame) {
+        if(dog.getDogRectangle().getX() != dog.getxBuffer() || dog.getDogRectangle().getY() != dog.getyBuffer())  {
+            game.batch.draw(
+                currentFrame,
+                dog.getDogRectangle().getX(),
+                dog.getDogRectangle().getY(),
+                dog.getDogRectangle().getWidth(),
+                dog.getDogRectangle().getHeight()
+            );
+
+            frameBuffer = currentFrame; // dans le cas ou le personnage bouge on actualise le frameBuffer
+            // pour pouvoir le figer quand il s'arrete.
+        } else {
+            game.batch.draw(
+                frameBuffer,
+                dog.getDogRectangle().getX(),
+                dog.getDogRectangle().getY(),
+                dog.getDogRectangle().getWidth(),
+                dog.getDogRectangle().getHeight()
+            );
+        }
+    }
+
+    private void drawReaper() {
+        if(timer.isNotWatching()) {
+            game.batch.draw(
+                    reaper.getGreenTexture(),
+                    reaper.getRectangle().x,
+                    reaper.getRectangle().y,
+                    reaper.getRectangle().width,
+                    reaper.getRectangle().height
+            );
+
+            this.playerXBuffer = dog.getDogRectangle().x;
+            this.playerYBuffer = dog.getDogRectangle().y;
+
+        } else if(timer.isReturning()) {
+            game.batch.draw(
+                    reaper.getOrangeTexture(),
+                    reaper.getRectangle().x,
+                    reaper.getRectangle().y,
+                    reaper.getRectangle().width,
+                    reaper.getRectangle().height
+            );
+
+            this.playerXBuffer = dog.getDogRectangle().x;
+            this.playerYBuffer = dog.getDogRectangle().y;
+
+        } else if(timer.isWatching()) {
+            game.batch.draw(
+                    reaper.getRedTexture(),
+                    reaper.getRectangle().x,
+                    reaper.getRectangle().y,
+                    reaper.getRectangle().width,
+                    reaper.getRectangle().height
+            );
+        }
     }
 
 }// Eo GameScreen class
